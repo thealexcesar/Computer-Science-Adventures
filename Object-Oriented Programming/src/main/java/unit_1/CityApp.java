6be9a06a306ece055a7b82790d0e983eda2a65b9
thealexcesar <thealexcesar@gmail.com>
2023-03-14 07:59:54 -0300

    clean

--- Object-Oriented Programming/src/main/java/unit_1/CityApp.java
+++
@@ -1,8 +0,0 @@
-package unit_1;
-
-public class CityApp {
-    public static void main(String[] args) {
-        City blumenau = new City("Blumenau", "SC", 157469, 151542, true);
-        System.out.println(blumenau.toString());
-    }
-}
